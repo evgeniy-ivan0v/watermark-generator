8b90d93ac53974d2874fc45beffdff64e4d3b93d
evgeniy-ivan0v <ivanov.j.1989@gmail.com>
2016-01-11 11:37:03 +0300

    немного поправлен js

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,9 +1,10 @@
 var devPanel = require('./module/dePanel.js');
 var opacity = require('./module/opacity.js');
 var uploadModule = require('./module/upload.js');
+var $ = require('jquery')
 
-
+$(document).ready(function() {
 	devPanel();
 	opacity();
 	uploadModule();
-	
+});
--- dev/js/module/opacity.js
+++ dev/js/module/opacity.js
@@ -1,22 +1,20 @@
 var $ = require('jquery');
-require('jquery-ui');
+require('jquery-ui/slider');
 
 var opacity = (function () {
 
     function _setUpListener() {
         
-        $(document).ready(function() {
-            $( ".opacity__slider" ).slider({
-                range: "min",
-                value: 50,
-                min: 0,
-                max: 100,
-                slide: function( event, ui ) {
-                    var value = ($( ".opacity__slider" ).slider( "value"))/100;
-                    $('.generator__watermark-image').css('opacity', value);
-                }
-            });
-        });    
+        $( ".opacity__slider" ).slider({
+            range: "min",
+            value: 50,
+            min: 0,
+            max: 100,
+            slide: function( event, ui ) {
+                var value = ($( ".opacity__slider" ).slider( "value"))/100;
+                $('.generator__watermark-image').css('opacity', value);
+            }
+        }); 
     };
 
   function init () {
